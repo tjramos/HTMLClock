51c1e94dfd4a198828b96677cb9a3091a1422025
unknown <timramos94@gmail.com>
2015-01-29 23:56:41 -0800

    lab4 commit

--- js/clock.js
+++ js/clock.js
@@ -3,3 +3,30 @@ function getTime() {
    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
    setTimeout(getTime, 1000);
 }
+
+function getTemp() {
+   $.getJSON("https://api.forecast.io/forecast/0f883d4fdb43b0ffeddc78b8594f743c/35.300399,-120.662362?callback=?",
+               function(data) {
+      $("#forecastLabel").html(data.daily.summary);
+      
+      $("#forecastIcon").attr("src", "img/" + data.daily.icon + ".png");
+      
+      var className;
+      var maxTemp = data.daily.temperatureMax;
+      
+      if(maxTemp < 60)
+         className = "cold";
+      else if(maxTemp >= 60 && maxTemp < 70)
+         className = "chilly";
+      else if(maxTemp >= 70 && maxTemp < 80)
+         className = "nice";
+      else if(maxTemp >= 80 && maxTemp < 90)
+         className = "warm";
+      else if(maxTemp >= 90)
+         className = "hot";
+      
+      $("body").addClass(className);
+   });
+   
+   console.log();
+}
